8a57b747597a0d70af7a445b59c2465d1b6e677a
misiekhardcore <misiekhardcore@gmail.com>
2020-12-16 11:41:25 +0100

    services section

--- src/constants/services.js
+++ src/constants/services.js
@@ -3,19 +3,19 @@ import { FaCode, FaSketch, FaAndroid } from "react-icons/fa"
 export default [
   {
     id: 1,
-    icon: <FaCode className="service-icon" />,
+    icon: <FaCode className="service__icon" />,
     title: "web development",
     text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
   },
   {
     id: 2,
-    icon: <FaSketch className="service-icon" />,
+    icon: <FaSketch className="service__icon" />,
     title: "web design",
     text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
   },
   {
     id: 3,
-    icon: <FaAndroid className="service-icon" />,
+    icon: <FaAndroid className="service__icon" />,
     title: "app design",
     text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
   },
