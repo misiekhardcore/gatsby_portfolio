8915f954cd5dc865b87f50ee0b19e4e5bc5c92d9
misiekhardcore <misiekhardcore@gmail.com>
2020-12-16 11:22:23 +0100

    layout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,19 @@ import logo from "../assets/logo.svg"
 import { FaAlignRight } from "react-icons/fa"
 import PageLinks from "../constants/links"
 const Navbar = () => {
-  return <h2>navbar component</h2>
+  return (
+    <nav className="navbar">
+      <div className="navbar--center">
+        <div className="navbar__header">
+          <img src={logo} alt="logo" />
+          <button className="toggleBtn">
+            <FaAlignRight />
+          </button>
+        </div>
+        <PageLinks styleClass="navbar__links" />
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
