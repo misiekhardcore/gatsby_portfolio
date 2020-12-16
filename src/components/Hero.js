3c816d85e569caf63b756146017d67e8b16393c1
misiekhardcore <misiekhardcore@gmail.com>
2020-12-16 11:22:16 +0100

    hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,9 +4,43 @@ import { Link } from "gatsby"
 import { graphql, useStaticQuery } from "gatsby"
 import SocialLinks from "../constants/socialLinks"
 // ...GatsbyImageSharpFluid
+const query = graphql`
+  {
+    file(relativePath: { eq: "hero-img.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
 
 const Hero = () => {
-  return <h2>hero component</h2>
+  const data = useStaticQuery(query)
+  const {
+    file: {
+      childImageSharp: { fluid: image },
+    },
+  } = data
+  return (
+    <div className="hero">
+      <section className="section--center hero--center">
+        <article className="hero__info">
+          <div>
+            <div className="underline"></div>
+            <h1>i'm john</h1>
+            <h4>freelancer, develoer</h4>
+            <Link to="/contact" className="btn">
+              contact me
+            </Link>
+            <SocialLinks />
+          </div>
+        </article>
+        <Image fluid={image} className="hero__img" />
+      </section>
+    </div>
+  )
 }
 
 export default Hero
--- src/constants/socialLinks.js
+++ src/constants/socialLinks.js
@@ -11,22 +11,22 @@ const data = [
   {
     id: 1,
     icon: <FaFacebookSquare className="social-icon"></FaFacebookSquare>,
-    url: "https://www.twitter.com",
+    url: "https://www.facebook.com",
   },
   {
     id: 2,
     icon: <FaLinkedin className="social-icon"></FaLinkedin>,
-    url: "https://www.twitter.com",
+    url: "https://www.linkedin.com",
   },
   {
     id: 3,
     icon: <FaDribbbleSquare className="social-icon"></FaDribbbleSquare>,
-    url: "https://www.twitter.com",
+    url: "https://www.dribble.com",
   },
   {
     id: 4,
     icon: <FaBehanceSquare className="social-icon"></FaBehanceSquare>,
-    url: "https://www.twitter.com",
+    url: "https://www.behance.net",
   },
   {
     id: 5,
@@ -37,7 +37,7 @@ const data = [
 const links = data.map(link => {
   return (
     <li key={link.id}>
-      <a href={link.url} className="social-link">
+      <a href={link.url} className="social-links__link">
         {link.icon}
       </a>
     </li>
