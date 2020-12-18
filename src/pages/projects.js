0223ed1694cafe6882fcbf4f508829e225f0a9ca
misiekhardcore <misiekhardcore@gmail.com>
2020-12-18 13:50:15 +0100

    projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,10 +2,42 @@ import React from "react"
 import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import Projects from "../components/Projects"
-// ...GatsbyImageSharpFluid
 
-const ProjectsPage = () => {
-  return <h2>projects page</h2>
+const ProjectsPage = ({ data }) => {
+  const {
+    allStrapiProjects: { nodes: projects },
+  } = data
+  return (
+    <Layout>
+      <Projects projects={projects} title="All projects" showLink/>
+    </Layout>
+  )
 }
+
+export const query = graphql`
+  {
+    allStrapiProjects {
+      nodes {
+        strapiId
+        github
+        featured
+        title
+        description
+        url
+        technologies {
+          title
+          id
+        }
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default ProjectsPage
