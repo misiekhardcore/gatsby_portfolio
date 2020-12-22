e1a803c04af5bdcc96289e949899fcca23845f1d
misiekhardcore <misiekhardcore@gmail.com>
2020-12-22 17:52:39 +0100

    blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,10 +2,41 @@ import React from "react"
 import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import Blogs from "../components/Blogs"
-// ...GatsbyImageSharpFluid
 
-const Blog = () => {
-  return <h2>blog page</h2>
+const Blog = ({
+  data: {
+    allStrapiBlogs: { nodes: blogs },
+  },
+}) => {
+  return (
+    <Layout>
+      <section className="blog__page">
+        <Blogs blogs={blogs} title="all blogs" />
+      </section>
+    </Layout>
+  )
 }
+
+export const query = graphql`
+  {
+    allStrapiBlogs(sort: { fields: date, order: DESC }, limit: 3) {
+      nodes {
+        date(formatString: "DD MMMM YYYY")
+        description
+        slug
+        title
+        category
+        id
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default Blog
