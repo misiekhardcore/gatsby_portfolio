b2baf64c0ebe090575669a34ad29089b25a461d5
misiekhardcore <misiekhardcore@gmail.com>
2020-12-22 23:51:10 +0100

    about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,9 +3,51 @@ import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import Title from "../components/Title"
 import Image from "gatsby-image"
-// ...GatsbyImageSharpFluid
-const About = () => {
-  return <h2>about page</h2>
+
+const About = ({ data }) => {
+  console.log(data)
+  const {
+    strapiAbout: { title, description, stack, image, id },
+  } = data
+  return (
+    <Layout>
+      <section className="about__page">
+        <div className="section--center about--center">
+          <Image fluid={image.childImageSharp.fluid} className="about__img" />
+          <article className="about__text">
+            <Title title={title} />
+            <p>{description}</p>
+            <div className="about__stack">
+              {stack.map(x => (
+                <span key={x.id}>{x.title}</span>
+              ))}
+            </div>
+          </article>
+        </div>
+      </section>
+    </Layout>
+  )
 }
+
+export const query = graphql`
+  {
+    strapiAbout {
+      description
+      id
+      image {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      title
+      stack {
+        id
+        title
+      }
+    }
+  }
+`
 
 export default About
