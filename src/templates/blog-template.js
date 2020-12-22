6cd2c446b913137cf2d2a9ac9cb62de5beaa4171
misiekhardcore <misiekhardcore@gmail.com>
2020-12-22 18:46:35 +0100

    blog pages generation

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -3,17 +3,31 @@ import { graphql, Link } from "gatsby"
 import Layout from "../components/Layout"
 import ReactMarkdown from "react-markdown"
 
-const ComponentName = () => {
-  return <h2>blog post template</h2>
+const ComponentName = ({ data }) => {
+  const {
+    blog: { content },
+  } = data
+  return (
+    <Layout>
+      <section className="blog__template">
+        <div className="section--center">
+          <article className="blog__content">
+            <ReactMarkdown source={content} />
+          </article>
+          <Link to='/blog' className='btn btn--center'>blog</Link>
+        </div>
+      </section>
+      
+    </Layout>
+  )
 }
 
-// export const query = graphql`
-//   query GetSingleBlog($slug: String) {
-//     blog: strapiBlogs(slug: { eq: $slug }) {
-//       content
-
-//     }
-//   }
-// `
+export const query = graphql`
+  query GetSingleBlog($slug: String) {
+    blog: strapiBlogs(slug: { eq: $slug }) {
+      content
+    }
+  }
+`
 
 export default ComponentName
