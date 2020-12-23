5b4f46ede0aa5b7f8dcc41bb95100167137bae34
misiekhardcore <misiekhardcore@gmail.com>
2020-12-23 14:02:32 +0100

    added SEO

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,6 +3,7 @@ import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import Title from "../components/Title"
 import Image from "gatsby-image"
+import SEO from "../components/SEO"
 
 const About = ({ data }) => {
   console.log(data)
@@ -11,6 +12,7 @@ const About = ({ data }) => {
   } = data
   return (
     <Layout>
+    <SEO title='About' description="About me"/>
       <section className="about__page">
         <div className="section--center about--center">
           <Image fluid={image.childImageSharp.fluid} className="about__img" />
