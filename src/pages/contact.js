02177a8a1caed9a859e41b2b69e710af9f3f8c2f
misiekhardcore <misiekhardcore@gmail.com>
2020-12-23 12:05:43 +0100

    contact page with formspree

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,40 @@ import React from "react"
 import Layout from "../components/Layout"
 
 const contact = () => {
-  return <h2>contact page</h2>
+  return (
+    <Layout>
+      <section className="contact__page">
+        <article className="contact__form">
+          <h3>get in touch</h3>
+          <form action="https://formspree.io/f/xayllonr" method="POST">
+            <div className="form-group">
+              <input
+                type="text"
+                name="name"
+                placeholder="name"
+                className="form-group__control"
+              />
+              <input
+                type="email"
+                name="email"
+                placeholder="email"
+                className="form-group__control"
+              />
+              <textarea
+                name="message"
+                rows="5"
+                placeholder="message"
+                className="form-group__control"
+              ></textarea>
+            </div>
+            <button type="submit" className="btn btn--submit">
+              sumbit here
+            </button>
+          </form>
+        </article>
+      </section>
+    </Layout>
+  )
 }
 
 export default contact
